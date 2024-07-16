ea8b99e0bfae5905ec5ae3de75456ca59d8c159c
bertomeu <josecarlosbertomeu@gmail.com>
2024-07-16 17:29:16 +0000

    Modified

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -4,10 +4,12 @@ const Navbar = () => {
     return (
         <nav class="navbar navbar-expand-lg mb-5" style={{ background: "#e2e3e5", borderBottom: "2px solid #000" }} >
             <div class="container-fluid">
-                <a class="navbar-brand d-flex align-items-center w-100" href="#">
-                    <img src="https://res.cloudinary.com/dyvut6idr/image/upload/v1720719412/Logo_4Geeks_Academy_qxozzt.png" alt="Logo" width="20%" height="20%" />
-                    <p className="text fs-1 ms-5" >Spain-FS-PT-73</p>
-                    <p className="name ms-auto" >by Jose Carlos Bertomeu</p>               
+                <a class="navbar-brand d-flex align-items-center w-100 justify-content-around" href="#">
+                    <img src="https://res.cloudinary.com/dyvut6idr/image/upload/v1720719412/Logo_4Geeks_Academy_qxozzt.png" alt="Logo" className="img-fluid" style={{ maxWidth: "20%", height: "auto" }} />
+                    <div className="ms-5">
+                        <p className="text fs-1 fs-md-3 mb-0">Spain-FS-PT-73</p>
+                        <p className="text fs-3 fs-md-3 mb-0">by Jose Carlos Bertomeu</p>
+                    </div>                
                 </a>    
             </div>
         </nav>
